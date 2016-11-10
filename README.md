cc81d9f3cbfa18bcd929a6961cfbb7ae1bc844e7
atf1999 <torth212@gmail.com>
2016-11-10 17:09:26 -0500

    Did more stuff

--- README.md
+++ README.md
@@ -18,8 +18,8 @@ Can return a value similar to a java ternary operator
 
 Ex: val canVote = if(age >= 18) "yes" else "no"
 
-with multiple conditions and outcomes
-
+With multiple conditions and outcomes
+```scala
 if((age >=5) && (age <= 6)){
   println("Go to kindergarten")
 } else if((age > 6) && (age <= 7)){
@@ -27,3 +27,16 @@ if((age >=5) && (age <= 6)){
 } else {
   println("Go to grade " + (age-5))
 }
+```
+## Function structure
+```scala
+def funcName(param1:dataType, param2:dataType) : dataType(Unit if no return) = {
+  function body
+  return valueToReturn
+}
+```
+can set default parameters by doing param:dataType = value
+
+##Infinite arguments
+In the function, for a parameter do ```param:dataType*```
+Must be last parameter of the function 
--- ScalaTutorial.scala
+++ ScalaTutorial.scala
@@ -1,3 +1,4 @@
+import scala.io.StdIn.{readLine, readInt};
 //object serves a similar purpose to the java file's main class of the file
 object ScalaTutorial{
   //Main method, similar to the java main method
@@ -41,5 +42,81 @@ object ScalaTutorial{
     for(i <- aList){
       println("List items " + i);
     }
+
+    //Filter example
+    //Yield stores the value(s) of i in the filter
+    println("\nFilter example (prints only even numbers from 1-20");
+    var evenList = for {i <- 1 to 20
+      if (i % 2) == 0
+    } yield i;
+
+    for(i <- evenList){
+      println(i);
+    }
+    
+    //Nested for loop
+    println("\nNested for loop:");
+    for(i <- 1 to 5; j <- 6 to 10){
+      println("i: " + i);
+      println("j: " + j);
+    }
+
+    //Function example
+    def printPrimes(){
+      println("\nFunction example:");
+      val primeList = List(1,2,3,5,7,11);
+      for(i <- primeList){
+        if(i == 11){
+          //return statement
+          return;
+        }
+
+        if(i != 1){
+          println(i);
+        }
+      }
+    }
+    printPrimes;
+
+    //Input example
+    println("\nInput example:");
+    var numberGuess = 0;
+
+    do{
+      //toInt converts string (in this case, readLine) to an integer
+      //readInt reads the line as an integer straight up
+      //Imported from the import line above
+      numberGuess = readLine("Guess the number: ").toInt;
+    }while(numberGuess != 15);
+
+    printf("You guessed the secret number %d\n", 15);
+
+    //arrays
+    val favNums = new Array[Int](20);
+    val friends = Array("Bob", "Tom");
+
+    friends(0) = "Sue";
+    println("\nBest friends " + friends(0));
+
+    //ArrayBuffer is an array whose size can change
+    val friends2 = ArrayBuffer[String]();
+    
+    //Different ways to add values to the ArrayBuffer
+    friends2.insert(0, "Phil");
+    friends2 += "Mark";
+    friends2 ++= Array("Susy", "Paul");
+    
+    //(startIndex, numOfItems) => removes the given number of items from the given start index
+    friends2.remove(1,2);
+    var friend : String = " ";
+    for(friend <- friends2){
+      println(friend);
+    }
+
+    //Map example
+    val favNumsTimes2 = for(num <- favNums) yield 2 *num
+
+    //for each shorthand
+    favNumsTimes2.foreach(println);
   }
 }
